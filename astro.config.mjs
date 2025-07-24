38075b5b8fac7d3af5890480e939fe40bdb7ede9
Kenneth Rose <rosekenny12@gmail.com>
2025-07-23 21:35:16 -0400

    Added favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://personal-astro.rosekenny12.workers.dev/',
   integrations: [mdx(), sitemap(), react()],
 
   vite: {
